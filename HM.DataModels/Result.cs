7eb635a67841ec2190c02767b9a8df12ef57b698
Trường Mai <truongmv@outlook.com>
2016-04-08 18:21:54 +0700

    Edit mongo DbContext

--- HM.DataModels/Result.cs
+++ HM.DataModels/Result.cs
@@ -4,30 +4,27 @@
     /// API sẽ trả về thông qua Result
     /// </summary>
     /// <typeparam name="T">Kiểu dữ liệu cụ thể sẽ được gọi</typeparam>
-    public class Result<T>
+    public class Result
     {
-        /// <summary>
-        /// Kết quả thành công hoặc thất bại khi gọi API
-        /// </summary>
-        public bool IsSuccess { get; set; }
-
         /// <summary>
         /// Mã lỗi (nếu có) khi gọi API thất bại
         /// </summary>
         public string Code { get; set; }
 
         /// <summary>
+        /// Kết quả thành công hoặc thất bại khi gọi API
+        /// </summary>
+        public bool IsSuccess()
+        {
+            return string.IsNullOrEmpty(Code);
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        /// <summary>
         /// Nếu không có lỗi, dữ liệu sẽ trả về qua biến DATA
         /// </summary>
         public T Data { get; set; }
-
-        /// <summary>
-        /// Hàm khởi tạo (hàm dựng)
-        /// </summary>
-        public Result()
-        {
-            IsSuccess = false;
-            Code = null;
-        }
     }
 }
---
+++ HM.DataModels/Utils/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.DataModels.Utils
+{
+    public static class CurrencyUtils
+    {
+        public const string CURRENCY_FORMAT_COMMA = "#,##0";
+        public const string CURRENCY_FORMAT_SPACE = "# ##0";
+    }
+
+    public static class DateTimeUtils
+    {
+        public const string ddMMyyyy = "dd-MM-yyyy";
+        public const string HHmm = "HH:mm";
+        public const string HHmmss = "HH:mm:ss";
+    }
+
+    public static class MessageUtils
+    {
+        public const string ERR_PERMISSION = "ERR_PERMISSION";
+        public const string ERR_NO_FIELD_TO_UPDATE = "ERR_NO_FIELD_TO_UPDATE";
+    }
+}
