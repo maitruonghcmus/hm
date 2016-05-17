8aea4a66a11ad07a4b28cf46e39acccd2810151d
Nhan Pham - 1542251 <pphoangnhan@gmail.com>
2016-05-18 01:59:43 +0700

    crud CustomerType, searchable for RoomType

---
+++ HM.WebApp/Controllers/CustomerTypeController.cs
@@ -0,0 +1,60 @@
+﻿using HM.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HM.WebApp.Controllers
+{
+    public class CustomerTypeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var ctmTypes = DataContext.Instance.GetCustomerTypes();
+            ViewBag.CustomerTypes = ctmTypes;
+            return View();
+        }
+
+
+        public ActionResult GetCustomerTypeInfo(int ctmTypeId)
+        {
+            var ctmType = DataContext.Instance.GetCustomerType(ctmTypeId);
+            return Json(ctmType, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCustomerType(CustomerType ctmType) 
+        {
+            var updateSuccess = DataContext.Instance.UpdateCustomerType(ctmType);
+            if (updateSuccess) { return Json(true, JsonRequestBehavior.AllowGet); }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult CreateCustomerType(CustomerType ctmType)
+        {
+            var createSuccess = DataContext.Instance.CreateCustomerType(ctmType);
+            if (createSuccess) { return Json(true, JsonRequestBehavior.AllowGet); }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomerType(int ctmTypeId)
+        {
+            var deleteSuccess = DataContext.Instance.DeleteCustomerType(ctmTypeId);
+            if (deleteSuccess) { return Json(true, JsonRequestBehavior.AllowGet); }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult LoadCustomerTypes()
+        {
+            var ctmTypes = DataContext.Instance.GetCustomerTypes();
+            ViewBag.CustomerTypes = ctmTypes;
+            return PartialView("_ShowCustomerTypes");
+        }
+
+
+    }
+}
