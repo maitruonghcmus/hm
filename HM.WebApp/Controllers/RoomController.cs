1413e94b2132e90fe04caeebf00b03e0d89d2a23
Nhan Pham - 1542251 <pphoangnhan@gmail.com>
2016-05-24 00:57:32 +0700

    Commit WebApp

--- HM.WebApp/Controllers/RoomController.cs
+++ HM.WebApp/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
-﻿using HM.WebApp.Models;
+﻿using HM.DataModels;
+using HM.WebApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,47 @@ namespace HM.WebApp.Controllers
         {
             var rooms = DataContext.Instance.GetRooms();
             var roomModels = rooms?.Select(a => new RoomModel(a));
+            var customers = DataContext.Instance.GetCustomers();
+            ViewBag.Customers = customers;
             ViewBag.Rooms = roomModels;
 
+
             return View();
+        }
+
+        public ActionResult LoadRooms()
+        {
+            var rooms = DataContext.Instance.GetRooms();
+            var roomModels = rooms?.Select(a => new RoomModel(a));
+            var customers = DataContext.Instance.GetCustomers();
+            ViewBag.Customers = customers;
+            ViewBag.Rooms = roomModels;
+
+
+            return PartialView("_ShowRooms");
+        }
+
+        public ActionResult GetRoomInfo(int rId)
+        {
+            var room = DataContext.Instance.GetRoom(rId);
+            var roommodel = new RoomModel(room);
+            return Json(roommodel,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult BookNewRoom(Order ord)
+        {
+            ord.CheckInDate = DateTime.Now;
+            var createsuccess = DataContext.Instance.CreateOrder(ord);
+
+            var r = DataContext.Instance.GetRoom(ord.RoomId);
+            r.Status = 1;
+
+            if (createsuccess)
+            {
+                var updatesuccess = DataContext.Instance.UpdateRoom(r);
+                if (updatesuccess) { return Json(true, JsonRequestBehavior.AllowGet); }
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
         }
     }
 }
